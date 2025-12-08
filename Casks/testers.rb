56e50a1a58e37a0fa6a954284984a8b9adb0b6ac
jasons-testersAI <jasons@testers.ai>
2025-12-07 16:55:14 -0800

    Update testers.rb

--- Casks/testers.rb
+++ Casks/testers.rb
@@ -4,7 +4,7 @@ cask "testers" do
   on_arm do
     sha256 "ffd080767fac1c9e6f9d11dffa5be93b8187c65ac4f296a1e660eaf71b8d61ae"
     url(
-      "https://github.com/testers-ai/releases/releases/download/v#{version}/testers-mac-arm64-#{version}.dmg",
+      "https://github.com/testers-ai/releases/releases/download/v#{version}/testers-mac-arm64-#{version}.zip",
       verified: "https://github.com/testers-ai/releases/"
     )
   end
@@ -12,7 +12,7 @@ cask "testers" do
   on_intel do
     sha256 "0f36120e76d333086dd22f862be8ad87b84b2b5ebc3a4d2e31b3720df53fc723"
     url(
-      "https://github.com/testers-ai/releases/releases/download/v#{version}/testers-mac-x86_64-#{version}.dmg",
+      "https://github.com/testers-ai/releases/releases/download/v#{version}/testers-mac-x86_64-#{version}.zip",
       verified: "https://github.com/testers-ai/releases/"
     )
   end
