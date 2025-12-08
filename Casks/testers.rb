237ec5a7821080f0f4c5ea3e93769f147ab9f07f
jasons-testersAI <jasons@testers.ai>
2025-12-07 16:04:42 -0800

    Update Homebrew cask for v0.9.13.5-2

--- Casks/testers.rb
+++ Casks/testers.rb
@@ -1,1 +1,34 @@
+cask "testers" do
+  version "0.9.13.5-2"
 
+  on_arm do
+    sha256 "ffd080767fac1c9e6f9d11dffa5be93b8187c65ac4f296a1e660eaf71b8d61ae"
+    url(
+      "https://github.com/testers-ai/releases/releases/download/v#{version}/testers-mac-arm64-#{version}.dmg",
+      verified: "https://github.com/testers-ai/releases/"
+    )
+  end
+
+  on_intel do
+    sha256 "0f36120e76d333086dd22f862be8ad87b84b2b5ebc3a4d2e31b3720df53fc723"
+    url(
+      "https://github.com/testers-ai/releases/releases/download/v#{version}/testers-mac-x86_64-#{version}.dmg",
+      verified: "https://github.com/testers-ai/releases/"
+    )
+  end
+
+  name "Testers.ai CLI"
+  desc "Automated web testing CLI"
+  homepage "https://testers.ai"
+
+  depends_on macos: :sonoma
+
+  livecheck do
+    url "https://github.com/testers-ai/releases"
+    strategy :github_latest
+  end
+
+  pkg "Install Testers.ai CLI.pkg"
+
+  uninstall pkgutil: "ai.testers.pkg"
+end
